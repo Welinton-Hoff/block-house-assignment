9ec6bba4c9e21f56099376a87186aa21bc796e2f
welinton <welinton.h.r@hotmail.com>
2025-01-26 17:27:55 +0000

    Fix ESLint configuration for flat config system
    - Replaced the "extends" key with direct configuration objects in eslint.config.js.
    - Updated to use ESLint 9.19.0 compatible format for React projects.
    - Ensured compatibility with both Android and iOS build pipelines.

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,19 @@
-module.exports = {
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  env: {
-    es6: true,
-    node: true,
-    browser: true,
+import eslintPluginReact from "eslint-plugin-react";
+
+export default [
+  {
+    files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+    },
+    plugins: {
+      react: eslintPluginReact,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
   },
-  plugins: ["react"],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-  },
-};
+];
